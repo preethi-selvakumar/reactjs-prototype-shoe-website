953b0e32d2e5301a837700f361f5d5668490b568
Preethi S <preethicoder90@outlook.com>
2025-09-05 20:37:57 +0530

    Update Home.jsx

--- shoe-website/src/pages/Home.jsx
+++ shoe-website/src/pages/Home.jsx
@@ -14,32 +14,25 @@ import BestPair from "../components/BestPair";
 import AdCarousel from "../components/AdCarousel";
 import HeroSection from "../components/HeroSection";
 
-
 const Home = () => {
     const navigate = useNavigate();
-    // Get isSignedUp and isLoggedIn from the global context
     const { isSignedUp, isLoggedIn } = useAppContext();
 
     useEffect(() => {
-        // First, check if the user has signed up
         if (!isSignedUp) {
             if (window.confirm("Please signup to continue")) {
                 navigate("/signup");
             }
         }
-        // If they are signed up but NOT logged in, then prompt them to log in
-        else if (isSignedUp && !isLoggedIn) {
+        else if (!isLoggedIn) {
             if (window.confirm("Please login to continue")) {
                 navigate("/login");
             }
         }
-        // If they are signed up AND logged in, do nothing. This is the correct state.
-
     }, [navigate, isSignedUp, isLoggedIn]);
 
     return (
         <div className="home-container">
-            {/* The rest of the page content will only render if the checks are passed implicitly */}
             <div className="home-content-wrapper">
                 <Navbar />
                 <HeroSection />
